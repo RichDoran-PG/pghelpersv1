830fd293f6ee533b34bb097402492a36d1772a4a
RichDoran-PG <rich.doran@partnerdgroup.com>
2025-04-12 11:36:43 -0400

    Update AutoUpdate.ps1
    Add force to Winget upgrade

--- AutoUpdate.ps1
+++ AutoUpdate.ps1
@@ -1,5 +1,5 @@
 Install-module pswindowsupdate -force -AllowClobber;
 get-windowsupdate -acceptall -install;
-winget upgrade --all;
+winget upgrade --all -force -AllowClobber;
 sfc /scannow;
 Restart-Computer
